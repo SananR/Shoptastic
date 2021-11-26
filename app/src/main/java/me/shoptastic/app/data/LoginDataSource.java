ef712dc21f149b0bcf202403c491101e328a3201
Ezzeldin Ismail <315zizx@gmail.com>
2021-11-25 22:05:52 -0500

    Changed LoggedInUser to User and added more fields

--- app/src/main/java/me/shoptastic/app/data/LoginDataSource.java
+++ app/src/main/java/me/shoptastic/app/data/LoginDataSource.java
@@ -1,6 +1,6 @@
 package me.shoptastic.app.data;
 
-import me.shoptastic.app.data.model.LoggedInUser;
+import me.shoptastic.app.data.model.User;
 
 import java.io.IOException;
 
@@ -8,16 +8,18 @@ import java.io.IOException;
  * Class that handles authentication w/ login credentials and retrieves user information.
  */
 public class LoginDataSource {
+    public LoginDataSource(){
+    }
 
-    public Result<LoggedInUser> login(String username, String password) {
+    public Result<User> login(String username, String password) {
 
         try {
             // TODO: handle loggedInUser authentication
-            LoggedInUser fakeUser =
-                    new LoggedInUser(
-                            java.util.UUID.randomUUID().toString(),
-                            "Jane Doe");
-            return new Result.Success<>(fakeUser);
+//            User fakeUser =
+//                    new User(
+//                            java.util.UUID.randomUUID().toString(),
+//                            "Jane Doe");
+            return new Result.Success<>(null);
         } catch (Exception e) {
             return new Result.Error(new IOException("Error logging in", e));
         }
