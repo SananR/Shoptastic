5adc76b164dc039e357a704702c047e93c605fef
Ezzeldin Ismail <315zizx@gmail.com>
2021-11-27 17:57:32 -0500

    Refactored login code to be more concise and more appropriate for the current app and less boilerplate-y.
    Now, the sign in button is always available, and the user data is not validated. When the user clicks the sign in button the data used to try and login the user and then a Result object is sent back with either an error or a success.

--- app/src/main/java/me/shoptastic/app/data/LoginDataSource.java
+++ app/src/main/java/me/shoptastic/app/data/LoginDataSource.java
@@ -1,28 +1,31 @@
 package me.shoptastic.app.data;
 
-import me.shoptastic.app.data.model.User;
+import com.google.firebase.database.FirebaseDatabase;
 
-import java.io.IOException;
+import me.shoptastic.app.R;
+import me.shoptastic.app.data.model.User;
 
 /**
  * Class that handles authentication w/ login credentials and retrieves user information.
  */
 public class LoginDataSource {
+    private final FirebaseDatabase firebaseDatabase;
     public LoginDataSource(){
+        firebaseDatabase = FirebaseDatabase.getInstance("https://shoptastic-6670d-default-rtdb.firebaseio.com/");
     }
 
     public Result<User> login(String username, String password) {
-
-        try {
-            // TODO: handle loggedInUser authentication
-//            User fakeUser =
-//                    new User(
-//                            java.util.UUID.randomUUID().toString(),
-//                            "Jane Doe");
-            return new Result.Success<>(null);
-        } catch (Exception e) {
-            return new Result.Error(new IOException("Error logging in", e));
-        }
+        return new Result.Error(new Exception(""), R.string.login_failed);
+//        try {
+//            // TODO: handle loggedInUser authentication
+////            User fakeUser =
+////                    new User(
+////                            java.util.UUID.randomUUID().toString(),
+////                            "Jane Doe");
+//            return new Result.Success<>(null);
+//        } catch (Exception e) {
+//            return new Result.Error(new IOException("Error logging in", e));
+//        }
     }
 
     public void logout() {
