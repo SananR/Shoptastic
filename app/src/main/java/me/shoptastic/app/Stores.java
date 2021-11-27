8a0e8ed362480d63417612a6233c5f0655360bb8
axxaxya <annanya21sharma@gmail.com>
2021-11-26 22:25:34 -0500

    Added log-in, sign-up page, account, stores, and products. Includes only basic features.

--- app/src/main/java/me/shoptastic/app/Stores.java
+++ app/src/main/java/me/shoptastic/app/Stores.java
@@ -3,13 +3,11 @@ package me.shoptastic.app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Stores extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
-
+        setContentView(R.layout.activity_stores);
     }
-
 }
