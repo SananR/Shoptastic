bed2bc371c406d82c4f3be4fbc623ac97f7695e5
manas khandelwal <manas.25.july@gmail.com>
2021-11-28 21:13:35 -0500

    Product class

--- app/src/main/java/me/shoptastic/app/data/model/Product.java
+++ app/src/main/java/me/shoptastic/app/data/model/Product.java
@@ -1,4 +1,34 @@
 package me.shoptastic.app.data.model;
 
+import android.graphics.Bitmap;
+
+
 public class Product {
+    private final String product_name;
+    Integer Id;
+    Integer Price;
+    String description;
+    Bitmap image;
+
+    public Product(String name, String description, Bitmap image, Integer Price, Integer Id) {
+        product_name = name;
+        this.description = description;
+        this.image = image;
+        this.Id = Id;
+        this.Price = Price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+       Product product = (Product) o;
+        return Id.equals(product.Id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Id;
+    }
 }
+
