9ee6cc2e63b5d815f6d6385faba61eb5298e6c26
axxaxya <annanya21sharma@gmail.com>
2021-11-28 23:51:59 -0500

    Added a page for Cart

--- app/src/main/java/me/shoptastic/app/Account.java
+++ app/src/main/java/me/shoptastic/app/Account.java
@@ -1,8 +1,12 @@
 package me.shoptastic.app;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import me.shoptastic.app.data.model.Customer;
+import me.shoptastic.app.data.model.User;
 
 public class Account extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class Account extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+    }
+
+    // This function is called when clicked acc details
+    public void account_details(View v){
+        User user = null;
+        if (user instanceof Customer){
+
+        }else{
+
+        }
     }
 }
