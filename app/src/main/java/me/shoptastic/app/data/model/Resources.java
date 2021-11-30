06dca0ded48780516698fb9a70af79403d2ab562
Ezzeldin Ismail <315zizx@gmail.com>
2021-11-29 23:39:51 -0500

    Added Resource object
    It is not easy to retrieve resources from the xml files outside activity classes, so this class was created to help facilitate that. It is meant to be instantiated in the first activity and used throughout the lifecycle of the application.
    
    This class should be used to retrieve resources anywhere that is not an activity.

---
+++ app/src/main/java/me/shoptastic/app/data/model/Resources.java
@@ -0,0 +1,56 @@
+package me.shoptastic.app.data.model;
+
+import android.content.Context;
+
+import java.lang.ref.WeakReference;
+
+public class Resources {
+    private static WeakReference<Context> context;
+
+    public static void setContext(Context context) {
+        Resources.context = new WeakReference<>(context.getApplicationContext());
+    }
+
+    public static String getString(Integer i) {
+        if (Resources.context == null)
+            throw new NullPointerException("Context was never initialized");
+        return Resources.context.get().getResources().getString(i);
+    }
+
+    public static String getString(Integer i, Object... format) {
+        if (Resources.context == null)
+            throw new NullPointerException("Context was never initialized");
+        return Resources.context.get().getResources().getString(i, format);
+    }
+
+    public static Boolean getBoolean(Integer i) {
+        if (Resources.context == null)
+            throw new NullPointerException("Context was never initialized");
+        return Resources.context.get().getResources().getBoolean(i);
+    }
+
+    public static Float getFloat(Integer i) {
+        if (Resources.context == null)
+            throw new NullPointerException("Context was never initialized");
+        return Resources.context.get().getResources().getFloat(i);
+    }
+
+    public static Integer getInteger(Integer i) {
+        if (Resources.context == null)
+            throw new NullPointerException("Context was never initialized");
+        return Resources.context.get().getResources().getInteger(i);
+    }
+
+    public static String[] getStringArray(Integer i) {
+        if (Resources.context == null)
+            throw new NullPointerException("Context was never initialized");
+        return Resources.context.get().getResources().getStringArray(i);
+    }
+
+    public static int[] getIntegerArray(Integer i) {
+        if (Resources.context == null)
+            throw new NullPointerException("Context was never initialized");
+        return Resources.context.get().getResources().getIntArray(i);
+    }
+
+}
