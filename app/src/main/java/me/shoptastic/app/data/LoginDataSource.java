ac056f96423eafb34aea48949facca5b3651f4e7
axxaxya <annanya21sharma@gmail.com>
2021-12-02 15:33:43 -0500

    renamed DefaultProfile.png to default_profile.png which was causing an error

--- app/src/main/java/me/shoptastic/app/data/LoginDataSource.java
+++ app/src/main/java/me/shoptastic/app/data/LoginDataSource.java
@@ -1,7 +1,16 @@
 package me.shoptastic.app.data;
 
 
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import me.shoptastic.app.R;
 import me.shoptastic.app.data.model.User;
@@ -16,11 +25,30 @@ public class LoginDataSource {
     public LoginDataSource() { fAuth = FirebaseAuth.getInstance(); }
 
     public Result<User> login(String username, String password) {
+        fAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("Test", "signInWithEmail:success");
+                            FirebaseUser user = fAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("Test", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
 
         return new Result.Error(new Exception(""), R.string.login_failed);
     }
 
     public void logout() {
         // TODO: revoke authentication
+        FirebaseAuth.getInstance().signOut();
     }
 }
