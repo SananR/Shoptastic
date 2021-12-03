e7df203a61a987cbcf486499add94580974dc8fe
Ezzeldin Ismail <315zizx@gmail.com>
2021-12-03 05:51:15 -0500

    Fixed/commented out errors in LoginDataSource

--- app/src/main/java/me/shoptastic/app/data/LoginDataSource.java
+++ app/src/main/java/me/shoptastic/app/data/LoginDataSource.java
@@ -2,7 +2,6 @@ package me.shoptastic.app.data;
 
 
 import android.util.Log;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
@@ -13,11 +12,6 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.io.IOException;
-
-import me.shoptastic.app.R;
-import me.shoptastic.app.SignIn;
-import me.shoptastic.app.data.model.Resources;
-import me.shoptastic.app.data.model.User;
 
 /**
  * Class that handles authentication w/ login credentials and retrieves user information.
@@ -30,8 +24,8 @@ public class LoginDataSource {
         fAuth = FirebaseAuth.getInstance();
     }
 
-    public Result<User> login(String email, String password) {
-        boolean value = false;
+    public Result login(String email, String password) {
+        final boolean[] value = {false};
         try {
             fAuth.signInWithEmailAndPassword(email, password)
                     .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
@@ -41,12 +35,11 @@ public class LoginDataSource {
                                 // Sign in success, update UI with the signed-in user's information
                                 Log.d("Test", "signInWithEmail:success");
                                 FirebaseUser user = fAuth.getCurrentUser();
-                                user.
-                                value = true;
+                                value[0] = true;
                             } else {
                                 // If sign in fails, display a message to the user.
                                 Log.w("Test", "signInWithEmail:failure", task.getException());
-                                value = false;
+                                value[0] = false;
                             }
                         }
                     });
@@ -54,15 +47,16 @@ public class LoginDataSource {
             // then pass on these values in the user
 
             // create presenter and then presenter will take them to the sign in page
-            if (value) return new Result.Success<>(user);
-            else return new Result.Error(new IOException("Error signing in the user"));
+            return null;
+//            if (value[0]) return new Result.Success<>(user);
+//            else return new Result.Error(new IOException("Error signing in the user"));
         } catch (Exception e) {
             return new Result.Error(new IOException("Error signing in", e));
         }
+    }
 
-        public void logout(){
-            // TODO: revoke authentication
-            FirebaseAuth.getInstance().signOut();
-        }
+    public void logout() {
+        // TODO: revoke authentication
+        FirebaseAuth.getInstance().signOut();
     }
 }
