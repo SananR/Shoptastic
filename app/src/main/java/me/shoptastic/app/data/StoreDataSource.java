bcc8c00c138e4b9c1718a4a189bb47b59e8ef48c
MirSafwatAyser <mirsafwatayser@gmail.com>
2021-12-03 01:50:48 -0500

    Made changes to the StoreDataSource class
    This class now directly interacts with Firebase and enters the store information into realtime database

--- app/src/main/java/me/shoptastic/app/data/StoreDataSource.java
+++ app/src/main/java/me/shoptastic/app/data/StoreDataSource.java
@@ -3,36 +3,45 @@ package me.shoptastic.app.data;
 
 import android.util.Log;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
+import java.io.IOException;
 
+import me.shoptastic.app.data.model.Customer;
+import me.shoptastic.app.data.model.Product;
 import me.shoptastic.app.data.model.Store;
+import me.shoptastic.app.data.model.StoreOwner;
+import me.shoptastic.app.data.model.User;
 
 public class StoreDataSource {
+    private final FirebaseAuth fAuth;
+    private final DatabaseReference dRef;
 
-    //Reading from the database. This is just basic implementation. Will need further changes.
-    DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Stores");
-    ValueEventListener listener = new ValueEventListener() {
+    public StoreDataSource() {
+        fAuth = FirebaseAuth.getInstance();
+        dRef = FirebaseDatabase.getInstance().getReference();
+    }
 
-
-        @Override
-        public void onDataChange(DataSnapshot dataSnapshot) {
-            Log.i("demo", "data changed");
-            for (DataSnapshot child : dataSnapshot.getChildren()) {
-                Store store = child.getValue(Store.class);
-                Log.i("demo", store.toString());
-            }
+    public Result<User> register(StoreOwner user, Store store, String password) {
+        try {
+            fAuth.createUserWithEmailAndPassword(user.getEmail(), password).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    //Registration successful
+                    String child;
+                    child = "stores";
+                    dRef.child(child).child(user.getUUID().toString()).setValue(store);
+                    LoginRepository.getInstance().setLoggedInUser(user);
+                } else {
+                    //TODO
+                    Log.d("TEST", task.getException().getMessage());
+                }
+            });
+            if (fAuth.getCurrentUser() != null) return new Result.Success<>(store);
+            else return new Result.Error(new IOException("Error creating Firebase store"));
+        } catch (Exception e) {
+            return new Result.Error(new IOException("Error registering new store", e));
         }
-
-        @Override
-        public void onCancelled(DatabaseError databaseError) {
-            Log.w("warning", "loadPost:onCancelled", databaseError.toException());
-        }
-    };
-
-
+    }
 }
