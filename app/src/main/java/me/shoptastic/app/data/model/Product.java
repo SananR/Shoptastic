9c2ca72d8bf090045396e241d79568bb36760425
Ezzeldin Ismail <315zizx@gmail.com>
2021-12-04 18:13:14 -0500

    Added missing function from merge

--- app/src/main/java/me/shoptastic/app/data/model/Product.java
+++ app/src/main/java/me/shoptastic/app/data/model/Product.java
@@ -1,11 +1,8 @@
 package me.shoptastic.app.data.model;
 
-import android.graphics.Bitmap;
-
-
 public class Product {
-    private String product_name;
-    private Integer Id;
+    private final String product_name;
+    private final Integer Id;
     private Integer Price;
     private String description;
     private int numberInCart;
@@ -22,21 +19,31 @@ public class Product {
         return this.Id;
     }
 
-    public Integer getPrice(){
+    public Integer getPrice() {
         return this.Price;
     }
 
-    public int getNumberInCart(){
+    public int getNumberInCart() {
         return this.numberInCart;
     }
 
-    public void changePrice(int Price){ this.Price = Price;}
-    public void changeDescription(String description){ this.description = description;}
+    public String getName() {
+        return this.product_name;
+    }
+
+    public void changePrice(int Price) {
+        this.Price = Price;
+    }
+
+    public void changeDescription(String description) {
+        this.description = description;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-       Product product = (Product) o;
+        Product product = (Product) o;
         return Id.equals(product.Id);
     }
 
