a5e245e110e9787e5992e8b0f6237e8edb7e1194
axxaxya <annanya21sharma@gmail.com>
2021-12-06 18:49:39 -0500

    Added pages for adding image

---
+++ app/src/main/java/me/shoptastic/app/AddingImage.java
@@ -0,0 +1,15 @@
+package me.shoptastic.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddingImage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adding_image);
+
+    }
+}
