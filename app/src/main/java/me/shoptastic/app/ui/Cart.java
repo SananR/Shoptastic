273ab1cfa04e20b5abdc5529d3590c419745f26d
Ezzeldin Ismail <315zizx@gmail.com>
2021-12-07 04:20:27 -0500

    Commented out erroenous code that was pushed

--- app/src/main/java/me/shoptastic/app/ui/Cart.java
+++ app/src/main/java/me/shoptastic/app/ui/Cart.java
@@ -1,18 +1,15 @@
-package me.shoptastic.app;
+package me.shoptastic.app.ui;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
-import android.widget.TextView;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
+import me.shoptastic.app.R;
 import me.shoptastic.app.data.model.Order;
 
 public class Cart extends AppCompatActivity {
@@ -29,42 +26,41 @@ public class Cart extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
 
-        order = new Order(this);
+        order = new Order("");
 
         initView();
         initList();
         calculateCart();
     }
 
-    private void bottomNavigation(){
-        FloatingActionButton floatingActionButton = findViewById(R.id.cart_btn);
-        LinearLayout homeBtn = findViewById(R.id.homeBtn);
+//    private void bottomNavigation(){
+//        FloatingActionButton floatingActionButton = findViewById(R.id.cart_btn);
+//        LinearLayout homeBtn = findViewById(R.id.homeBtn);
+//
+//        floatingActionButton.setOnClickListener((new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v){
+//                startActivity(new Intent(Cart.this, Cart.class));
+//            }
+//        }));
+//
+//        homeBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v){
+//                startActivity(new Intent(Cart.this, Cart.class));
+//            }
+//        });
+//    }
 
-        floatingActionButton.setOnClickListener((new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                startActivity(new Intent(Cart.this, Cart.class));
-            }
-        }));
-
-        homeBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                startActivity(new Intent(Cart.this, Cart.class));
-            }
-        });
-    }
-
-    private void initList(){
+    private void initList() {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         recyclerViewList.setLayoutManager(linearLayoutManager);
         //adapter =
-                recyclerViewList.setAdapter(adapter);
-        if(order.getListCart().isEmpty()){
+        recyclerViewList.setAdapter(adapter);
+        if (order.getListCart().isEmpty()) {
             emptyTxt.setVisibility(View.VISIBLE);
             scrollView.setVisibility(View.GONE);
-        }
-        else{
+        } else {
             emptyTxt.setVisibility(View.GONE);
             scrollView.setVisibility(View.VISIBLE);
         }
@@ -72,7 +68,7 @@ public class Cart extends AppCompatActivity {
     }
 
     private void calculateCart(){
-        int itemTotal = order.getSumPrice();
+        Float itemTotal = order.getSumPrice();
         totalFeeTxt.setText("$" + itemTotal);
     }
 
--- app/src/main/java/me/shoptastic/app/ui/SProducts.java
+++ app/src/main/java/me/shoptastic/app/ui/SProducts.java
@@ -1,19 +1,14 @@
 package me.shoptastic.app;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 import java.util.ArrayList;
 
-import me.shoptastic.app.Adapter.ProductAdapter;
+import me.shoptastic.app.adapter.ProductAdapter;
 import me.shoptastic.app.data.model.Product;
 
 public class SProducts extends AppCompatActivity {
@@ -28,37 +23,37 @@ public class SProducts extends AppCompatActivity {
         recyclerView();
     }
 
-    private void bottomNavigation(){
-        FloatingActionButton floatingActionButton = findViewById(R.id.cart_btn);
-        LinearLayout homeBtn = findViewById(R.id.homeBtn);
+//    private void bottomNavigation(){
+//        FloatingActionButton floatingActionButton = findViewById(R.id.cart_btn);
+//        LinearLayout homeBtn = findViewById(R.id.homeBtn);
+//
+//        floatingActionButton.setOnClickListener((new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v){
+//                startActivity(new Intent(SProducts.this, Cart.class));
+//            }
+//        }));
+//
+//        homeBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v){
+//                startActivity(new Intent(SProducts.this, Cart.class));
+//            }
+//        });
+//    }
 
-        floatingActionButton.setOnClickListener((new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                startActivity(new Intent(SProducts.this, Cart.class));
-            }
-        }));
-
-        homeBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                startActivity(new Intent(SProducts.this, Cart.class));
-            }
-        });
-    }
-
-    private void recyclerView(){
+    private void recyclerView() {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         recyclerViewList = findViewById(R.id.recyclerView);
         recyclerViewList.setLayoutManager(linearLayoutManager);
 
         ArrayList<Product> products = new ArrayList<>();
         //demo
-        products.add(new Product("Apple", "Good", 2,87));
-        products.add(new Product("Pineapple", "Fresh", 1,28));
-        products.add(new Product("Phone", "New", 374,28));
-        products.add(new Product("Onion", "Veggies", 47,28));
-        products.add(new Product("Notes", "Useful", 21,28));
+        products.add(new Product("Apple", "Good", 2f, 87));
+        products.add(new Product("Pineapple", "Fresh", 1f, 28));
+        products.add(new Product("Phone", "New", 374f, 28));
+        products.add(new Product("Onion", "Veggies", 47f, 28));
+        products.add(new Product("Notes", "Useful", 21f, 28));
         adapter = new ProductAdapter(products);
         recyclerViewList.setAdapter(adapter);
 
