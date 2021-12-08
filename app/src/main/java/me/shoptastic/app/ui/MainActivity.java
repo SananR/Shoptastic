b9e025a8883609e06bbcbdea1dc49893a3733a7a
Ezzeldin Ismail <315zizx@gmail.com>
2021-12-08 17:42:50 -0500

    Improved design of store repository

--- app/src/main/java/me/shoptastic/app/ui/MainActivity.java
+++ app/src/main/java/me/shoptastic/app/ui/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        StoreRepository.getInstance().setup();
+        StoreRepository.getInstance();
         Intent i = new Intent(this, LoginActivity.class);
         startActivity(i);
     }
