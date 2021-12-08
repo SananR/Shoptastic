175c522339b8416d2e8d7f2602cf3475a4e84f8d
Ezzeldin Ismail <315zizx@gmail.com>
2021-12-08 01:47:23 -0500

    Removed unnecessary file and fixed error in owner_post_dish

--- app/src/main/java/me/shoptastic/app/ui/ProductAddActivity.java
+++ app/src/main/java/me/shoptastic/app/ui/ProductAddActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import me.shoptastic.app.R;
 import me.shoptastic.app.data.presenter.ProductPresenter;
@@ -14,7 +15,7 @@ public class ProductAddActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_addproduct);
+        setContentView(R.layout.activity_owner_post_dish);
         Button button = findViewById(R.id.post);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -25,19 +26,19 @@ public class ProductAddActivity extends Activity {
     }
 
     public String getProductName() {
-        return ((EditText) findViewById(R.id.qt2)).getText().toString();
+        return ((TextView) findViewById(R.id.addProductName)).getText().toString();
     }
 
     public String getProductDescription() {
-        return ((EditText) findViewById(R.id.Description)).getText().toString();
+        return ((EditText) findViewById(R.id.addProductDescription)).getText().toString();
     }
 
     public Float getProductPrice() {
-        return Float.valueOf(((EditText) findViewById(R.id.qt4)).getText().toString());
+        return Float.valueOf(((EditText) findViewById(R.id.addProductPrice)).getText().toString());
     }
 
     public Integer getProductID() {
-        return Integer.valueOf(((EditText) findViewById(R.id.qt)).getText().toString());
+        return Integer.valueOf(((EditText) findViewById(R.id.addProductID)).getText().toString());
     }
 
     public String getStoreName() {
@@ -45,10 +46,10 @@ public class ProductAddActivity extends Activity {
     }
 
     public void clear() {
-        ((EditText) findViewById(R.id.qt2)).setText("");
-        ((EditText) findViewById(R.id.Description)).setText("");
-        ((EditText) findViewById(R.id.qt4)).setText("");
-        ((EditText) findViewById(R.id.qt)).setText("");
+        ((EditText) findViewById(R.id.addProductName)).setText("");
+        ((EditText) findViewById(R.id.addProductPrice)).setText("");
+        ((EditText) findViewById(R.id.addProductID)).setText("");
+        ((EditText) findViewById(R.id.addProductDescription)).setText("");
     }
 
     public void register(View v) {
