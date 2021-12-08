f1f6ee2c4902bb3871c1561960035fc019aa166a
Ezzeldin Ismail <315zizx@gmail.com>
2021-12-08 00:45:21 -0500

    Implemented Products view

--- app/src/main/java/me/shoptastic/app/adapter/ProductAdapter.java
+++ app/src/main/java/me/shoptastic/app/adapter/ProductAdapter.java
@@ -2,7 +2,6 @@ package me.shoptastic.app.adapter;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,34 +14,40 @@ import java.util.ArrayList;
 
 import me.shoptastic.app.R;
 import me.shoptastic.app.data.model.Product;
+import me.shoptastic.app.ui.Products;
 import me.shoptastic.app.ui.ShowDetailActivity;
 
 public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHolder>{
     ArrayList<Product> productDomains;
+    Products view;
 
-    public ProductAdapter (ArrayList<Product> productDomains){
+    public ProductAdapter(Products view, ArrayList<Product> productDomains) {
         this.productDomains = productDomains;
+        this.view = view;
     }
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
-        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_products, parent, false);
-
-        return new ViewHolder(inflate);
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(this.view);
+        View view = inflater.inflate(R.layout.viewholder_products, parent, false);
+        return new ProductAdapter.ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, @SuppressLint("RecyclerView") final int position){
-        holder.title.setText(productDomains.get(position).getName());
-        holder.fee.setText(String.valueOf(productDomains.get(position).getPrice()));
-        holder.addBtn.setOnClickListener(new View.OnClickListener(){
+    public void onBindViewHolder(@NonNull ViewHolder holder, @SuppressLint("RecyclerView") final int position) {
+        final Product product = productDomains.get(position);
+        holder.title.setText(product.getName());
+        holder.fee.setText(String.valueOf(product.getPrice()));
+        holder.addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 Intent intent = new Intent(holder.itemView.getContext(), ShowDetailActivity.class);
-                intent.putExtra("productName", productDomains.get(position).getName());
-                intent.putExtra("productPrice", productDomains.get(position).getPrice().toString());
-                intent.putExtra("description", productDomains.get(position).getDescription());
+                intent.putExtra(Products.productName, product.getName());
+                intent.putExtra(Products.productPrice, product.getPrice().toString());
+                intent.putExtra(Products.productDescription, product.getDescription());
+                intent.putExtra(Products.productID, product.getId());
+                intent.putExtra(Products.productStore, view.getStoreName());
                 holder.itemView.getContext().startActivity(intent);
             }
 
